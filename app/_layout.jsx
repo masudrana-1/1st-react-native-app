d47498affdaad1ddb51216115a87c72f4afcaea6
masudrana-1 <masudrana58855@gmail.com>
2024-05-02 10:45:49 +0600

    fixed some error

---
+++ app/_layout.jsx
@@ -0,0 +1,9 @@
+import { Slot } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+const RootLayout = () => {
+    return <Slot/>
+}
+
+export default RootLayout
--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Masud</Text>
+            <Text>Masud Rana</Text>
             <StatusBar style="auto" />
         </View>
     );
