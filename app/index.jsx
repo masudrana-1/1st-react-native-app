3cd88527b7c4b46730587bf341d27e06fdb21494
masudrana-1 <masudrana58855@gmail.com>
2024-05-03 09:06:22 +0600

    load all fonts

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
     return (
