179bd99c1d7773b68621d754932cc3afa0f852c1
masudrana-1 <masudrana58855@gmail.com>
2024-05-06 11:23:42 +0600

    added scrollview for screen scroll

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
     return (
         <SafeAreaView className="bg-primary h-full">
-
+            <ScrollView contentContainerStyle={{ height: '100%' }}>
+                
+            </ScrollView>
         </SafeAreaView>
     );
 }
