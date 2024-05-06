4563c5f439a1b5368ec2d159f6bcbc7c3eff6311
masudrana-1 <masudrana58855@gmail.com>
2024-05-06 12:33:22 +0600

    style btn

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -9,10 +9,15 @@ const CustomButton = ({
     isLoading
 }) => {
     return (
-        <TouchableOpacity className={`bg-secondary rounded-xl min-h-[62px] flex flex-row justify-center items-center ${containerStyles} ${
-        isLoading ? "opacity-50" : ""
-        }`}>
-            <Text>CustomButton</Text>
+        <TouchableOpacity
+            onPress={handlePress}
+            activeOpacity={0.7}
+            className={`bg-secondary rounded-xl min-h-[62px] flex flex-row justify-center items-center ${containerStyles} ${
+            isLoading ? "opacity-50" : ""
+                }`}
+            disabled={isLoading}
+        >
+            <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>{ title }</Text>
         </TouchableOpacity>
     )
 }
