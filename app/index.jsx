2a731c2f1e3f0770f47138664179c10bd7c8f51d
masudrana-1 <masudrana58855@gmail.com>
2024-05-06 11:22:04 +0600

    added saveAreaView

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,13 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
     return (
-        <View className="flex-1 items-center justify-center">
-            <Text className="text-3xl font-pblack">Masud Rana!</Text>
-            <StatusBar style="auto" />
-            <Link href="/home" style={{color: 'blue'}}>Go to home</Link>
-        </View>
+        <SafeAreaView className="bg-primary h-full">
+
+        </SafeAreaView>
     );
 }
 
